c96072a59d539380d8d9b62cc2182d5572026a27
Webwiz <aliuanas004@gmail.com>
2023-07-16 17:58:37 +0100

    day 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const jsxElement = <h1>THis is a JSX element</h1>
+
+
+const welcome = 'Welcome to 30 days of React'
+const title = 'Getting Started React'
+const subtitle = 'JavaSript Library'
+const author = {
+    firstname: 'Anas',
+    lastName: 'Aliu',
+}
+const date = '14th July, 2023'
+
+const header = (
+    <header>
+        <div className='header-wrapper'>
+            <h1>{welcome}</h1>
+            <h2>{title}</h2>
+            <h3>{subtitle}</h3>
+            <p>Instructor: {author.firstname} {author.lastName}</p>
+            <small>Date: {date}</small>
+        </div>
+    </header>
+)
+
+const yearBorn = 2000
+const currentYear = new Date().getFullYear()
+const age = currentYear - yearBorn
+const personAge = (
+    <p>
+        {' '}
+        {author.firstname} {author.lastName} is {age} years Old
+    </p>
+)
+
+
+const techs = ['HTML', 'CSS', 'JavaScript']
+const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+
+const main = (
+    <main>
+        <div className='main-wrapper'>
+            <p>
+                Prerequisite to get started{' '}
+                <strong>
+                    <em>react.js</em>
+                </strong>
+                :
+            </p>
+            <ul>{techsFormatted}</ul>
+            {personAge}
+        </div>
+    </main>
+)
+
+const copyRight = 'Copyright 2020.'
+
+const footer = (
+    <footer>
+        <div className='footer-wrapper'>
+            <p>
+                {copyRight}
+            </p>
+        </div>
+    </footer>
+)
+
+const textStyle = {
+    textAlign: 'center',
+    fontSize: "20px",
+    margin: "20px",
+}
+
+const sub = (
+    <div class='bodySub'>
+        <div style={textStyle} class='txt'><h1>SUBCRIBE</h1></div>
+        
+        <p>Sign up with your email address to recieve news and update</p>
+
+        <div>
+            <input type="text" name="firstname" placeholder='firstname' id="" />
+            <input type="text" name="lastname" placeholder='lastname' id="" />
+            <input type="email" name="e-mail" placeholder='e-mail' id="" />
+        </div>
+        <div>
+            <button>Subscribe</button>
+        </div>
+    </div>
+)
+
+const app = (
+    <div className='app'>
+        {header}
+        {main}
+        {sub}
+        {footer}
+    </div>
+)
+
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(jsxElement, rootElement)
+ReactDOM.render(app, rootElement)
